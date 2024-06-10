6c824671d1be4b51a92aa2c8ac25d37fc3ba3fa3
Oreoluwa Ajayi <oreoluwaajayyiruth@gmail.com>
2024-06-10 06:08:48 +0100

    feat: project setup

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className='h-[100vh] w-full bg-black text-white justify-center items-center flex'>LandingPage</div>
+  )
+}
+
+export default LandingPage
